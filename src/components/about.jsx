1f094dafbcb95092d3ce3398cf7b24220de0ab00
Rev-Ant <208187306+Rev-Ant@users.noreply.github.com>
2025-11-26 22:57:32 +0000

    Finished the Intro component, create a header component using NavBar from bootstrap, create a about componet as well. Edited the default css files, app.css and index.css.

---
+++ src/components/about.jsx
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap"
+
+function About() {
+	return (
+		<Container
+			fluid
+			style={{
+				backgroundColor: 'blue',
+				color: 'white'
+			}}>
+			<h1>About me</h1>
+			<p>
+				Sou um estudante de Engenharia Informática
+
+			</p>
+		</Container>
+	)
+}
+
+export default About
---
+++ src/components/intro.jsx
@@ -0,0 +1,60 @@
+import { Image, Button, Container, Col, Row } from 'react-bootstrap'
+import clippy from './../assets/señor_clippy.png'
+import curriculo from '/home/ant/Documents/Private_Projects/Portfolio/src/assets/Real-World-Bug-Hunting.pdf'
+import background from '/home/ant/Downloads/image_4.jpg'
+import { FaDownload } from 'react-icons/fa'
+import { ReactTyped } from 'react-typed'
+
+function Intro() {
+	return (
+		<Container
+			fluid
+			style={{
+				// backgroundColor: "black",
+				// color: "white",
+			}}
+		>
+			<Row
+				className='d-flex justify-content-center'
+				style={{
+					// backgroundColor: 'red',
+					backgroundImage: `url(${background})`,
+					backgroundSize: "cover",
+					backgroundPosition: 'center',
+					backgroundRepeat: 'no-repeat',
+					minHeight: '100vh',
+					alignItems: 'center'
+				}}
+			>
+				<Col style={{ maxWidth: '650px' }}>
+					<Image src={clippy}
+						roundedCircle
+						style={{ width: "300px", height: "300px", objectFit: "cover", marginBottom: "15px" }}
+					/>
+				</Col>
+				<Col style={{ maxWidth: '650px' }}>
+					<h3>
+						<ReactTyped
+							strings={['Olá, eu sou o Manuel Albuquerque']}
+							typeSpeed={50}
+						/>
+					</h3>
+					<p>
+						Um estudante de Engenharia Informática que gosta de fazer projetos
+						com diferentes tecnologias para aprender e evoluir constantemente.
+					</p>
+					<Button
+						variant="light"
+						href={curriculo}
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<FaDownload style={{ marginRight: "8px" }} /> Curriculum (pdf)
+					</Button>
+				</Col>
+			</Row>
+		</Container>
+	)
+}
+
+export default Intro
